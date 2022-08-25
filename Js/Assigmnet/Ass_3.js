e60fc4368e70c31b1cdeac3990fe810b39c0c706
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-08-25 15:46:40 +0300

    assigment completed

---
+++ Js/Assigmnet/Ass_3.js
@@ -0,0 +1,29 @@
+//! ODEV1: tarkan string'indeki kelimelerin sayisini donduren
+//! bir fonksiyon yaziniz.
+//! const tarkan = "Gel gunduzle gece olalim";
+
+const tarkan = "Gel gunduzle gece olalim";
+newTarkan = tarkan.split(" ");
+console.log(
+  `tarkan string'inin içerisinde ${newTarkan.length} adet kelime bulunmatadır.`
+);
+
+for (let i = 0; i < newTarkan.length; ++i) {
+  wordTarkan = newTarkan[i].length;
+  console.log(`${i + 1}. kelimenin uzunluğu ${wordTarkan}'dür/dır.`);
+}
+//! ODEV2:
+//! ----------------------------------------------------------
+//! https://www.youtube.com/watch?v=b7vfp5G4brE
+//! https://youtu.be/b7vfp5G4brE
+
+const linkYoutube = `https://www.youtube.com/watch?v=b7vfp5G4brE`;
+
+const equal = /[=]/;
+const www = /[www]/;
+
+const shortLink =
+  linkYoutube.slice(0, linkYoutube.search(www)) +
+  `youtu.be/` +
+  linkYoutube.slice(linkYoutube.search(equal) + 1);
+console.log(shortLink);
