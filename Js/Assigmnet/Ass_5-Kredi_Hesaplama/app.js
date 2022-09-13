791cf521ba0b2bc0b6bc1e178b0245483c52e5c1
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-09-14 01:29:29 +0300

    js edited

--- Js/Assigmnet/Ass_5-Kredi_Hesaplama/app.js
+++ Js/Assigmnet/Ass_5-Kredi_Hesaplama/app.js
@@ -1,10 +1,48 @@
-//? Selector
-const krediDom = document.querySelectorAll("option");
+const krediDom = document.querySelector("#kredi");
 const vadeDom = document.getElementById("vade");
 const tutarDom = document.querySelector("#tutar");
 const hesaplaDom = document.querySelector("#hesapla");
+const tabloDom = document.querySelector("#tablo");
+
+//  Kredi Tutarı * [Faiz * (1+Faiz)Taksit Sayısı / (1+Faiz)Taksit Sayısı-1] formülü ile hesaplanır.
 
 hesaplaDom.addEventListener("click", () => {
-  console.log(krediDom);
-  console.log(vadeDom);
+  let oran;
+  if (krediDom.value === "Seçiniz") {
+    alert("Lütfen kredi türünü seçiniz.");
+  } else if (krediDom.value === "Konut Kredisi") {
+    oran = 1.29;
+  } else if (krediDom.value === "İhtiyaç Kredisi") {
+    oran = 1.99;
+  } else if (krediDom.value === "Araç Kredisi") {
+    oran = 1.79;
+  }
+  let vade = Number(vadeDom.value);
+  let tutar = Number(tutarDom.value);
+  let faiz = oran / 100;
+  let taksitTutarı =
+    (tutar * (faiz * (1 + faiz) ** vade)) / ((1 + faiz) ** vade - 1);
+  let toplamTutar = taksitTutarı * vade;
+  console.log(oran, toplamTutar, taksitTutarı);
+
+  tabloDom.innerHTML = `<tbody>
+            <tr>
+              <th scope="row">Kredi Miktarı</th>
+              <td>${tutar}</td>
+              <th>Kredi Tipi</th>
+              <td>${krediDom.value}</td>
+            </tr>
+            <tr>
+              <th scope="row">Vade</th>
+              <td>${vade}</td>
+              <th>Faiz Oranı</th>
+              <td>${oran}</td>
+            </tr>
+            <tr>
+              <th scope="row">Toplam Tutar</th>
+              <td>${toplamTutar.toFixed(2)} ₺</td>
+              <th>Taksit Tutarı</th>
+              <td>${taksitTutarı.toFixed(2)} ₺</td>
+            </tr>
+          </tbody>`;
 });
