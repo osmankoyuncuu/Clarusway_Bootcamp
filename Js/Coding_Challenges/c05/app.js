7e401db4a4f80cee5ce21da242fa339f6502f294
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-09-15 00:20:09 +0300

    finish

---
+++ Js/Coding_Challenges/c05/app.js
@@ -0,0 +1,33 @@
+const text = document.querySelector("#textArea");
+const buttonDom = document.querySelector("#button");
+const resetDom = document.querySelector("#reset");
+const resultDom = document.querySelector("#result");
+
+const vowel = ["a", "e", "ı", "i", "o", "ö", "u", "ü"];
+
+buttonDom.addEventListener("click", (event) => {
+  event.preventDefault();
+  counter = 0;
+  for (i of text.value) {
+    if (vowel.includes(i)) {
+      counter++;
+    }
+  }
+  if (text.value == 0) {
+  } else {
+    const paragraf = document.createElement("p");
+    resultDom.appendChild(paragraf);
+    paragraf.innerHTML = `The are ${counter} vowel in <span>   ${text.value}</span>`;
+    text.value = "";
+  }
+});
+
+text.addEventListener("keydown", (event) => {
+  if (event.keyCode == 13) {
+    buttonDom.click();
+  }
+});
+resetDom.addEventListener("click", () => {
+  window.location.reload();
+});
+text.focus();
